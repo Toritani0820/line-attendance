0759f107578b327081bfc7cd1051ae764159ce6f
Toritani0820 <ntoritani@gmail.com>
2026-07-26 18:56:09 +0900

    Create config.js

---
+++ config.js
@@ -0,0 +1,17 @@
+const CONFIG = {
+    // GAS関連の設定
+    GAS_URL: "ここにデプロイしたGASのウェブアプリURLを貼り付けてください",
+    
+    // LINE関連の設定
+    LIFF_ID: "ここにLINE Developersで発行したLIFF_IDを貼り付けてください",
+    
+    // 管理者・権限の定義
+    ROLES: {
+        SYSTEM_ADMIN: "システム管理者",
+        OPERATION_ADMIN: "運用管理者",
+        HOUSEHOLD_ADMIN: "世帯管理者",
+        RESPONDENT: "予定回答者",
+        VIEWER: "閲覧者",
+        BANNED: "【利用禁止】"
+    }
+};
